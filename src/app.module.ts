47e315483ede2c864bd4f5c3722cfbbfbf99ddf5
Kauã Steliano <stelianok@gmail.com>
2024-07-08 14:00:46 -0300

    Removed unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
@@ -7,7 +6,7 @@ import { FeatureModule } from './feature/feature.module';
 
 @Module({
   imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true }), FeatureModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule { }
